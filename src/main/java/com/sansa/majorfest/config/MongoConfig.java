108ae68ff79c4c9262f28f2c6510c54316d255ed
PauSansa <pausansa59@gmail.com>
2023-08-24 01:05:40 +0200

    fix mongo connection and mock

--- src/main/java/com/sansa/majorfest/config/MongoConfig.java
+++ src/main/java/com/sansa/majorfest/config/MongoConfig.java
@@ -10,18 +10,18 @@ import com.mongodb.client.MongoClients;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+    String mongoDbUrl = System.getenv("MONGO_DB_URL");
+    String username = System.getenv("MONGO_DB_USERNAME");
+    String password = System.getenv("MONGO_DB_PASSWORD");
+    String dbName = System.getenv("MONGO_DB_DATABASE");
 
     @Override
     protected String getDatabaseName() {
-        return "my-database";
+        return dbName;
     }
 
     @Override
     public MongoClient mongoClient() {
-        String mongoDbUrl = System.getenv("MONGO_DB_URL");
-        String username = System.getenv("MONGO_DB_USERNAME");
-        String password = System.getenv("MONGO_DB_PASSWORD");
-        String dbName = System.getenv("MONGO_DB_DATABASE");
 
         if (mongoDbUrl == null || username == null || password == null || dbName == null) {
             throw new IllegalStateException("MONGO_DB_URL, MONGO_DB_USERNAME and MONGO_DB_PASSWORD must be set");
@@ -30,14 +30,12 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         String uri = "mongodb://" + username + ":" + password + "@" + mongoDbUrl + "/" + dbName;
 
         ConnectionString connectionString = new ConnectionString(uri);
-        System.out.println(connectionString.getDatabase());
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .uuidRepresentation(UuidRepresentation.STANDARD)
                 .build();
-        MongoClient mongoClient = MongoClients.create(settings);
 
-        return mongoClient;
+        return MongoClients.create(settings);
     }
 }
 
